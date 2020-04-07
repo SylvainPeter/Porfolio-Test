e3e8d64814c3a5037d8c901e99927f147b9b28fb
Sylvain Peter <lexriders@gmail.com>
2020-04-07 10:49:06 +0200

    Ajoute animations skills

--- js/main.js
+++ js/main.js
@@ -49,12 +49,57 @@ e.addEventListener ("mouseleave", event => {
 */
 
 
-const a = document.querySelector('.logo');
+const a = document.querySelector('#html5');
+const b = document.querySelector('#css3');
+const c = document.querySelector('#sass');
+const d = document.querySelector('#bootstrap4');
+const e = document.querySelector('#javascript');
+const f = document.querySelector('#versionning');
 
-a.addEventListener ("click", event => {
-	a.classList.add('animated', 'bounce')
+a.addEventListener ("mouseenter", event => {
+	a.classList.add('animated', 'heartBeat')
 	});
 
-a.addEventListener ("animationend", event => {
-	a.classList.remove('animated', 'bounce')
+a.addEventListener ("mouseleave", event => {
+	a.classList.remove('animated', 'heartBeat')
+	});
+
+b.addEventListener ("mouseenter", event => {
+	b.classList.add('animated', 'heartBeat')
+	});
+
+b.addEventListener ("mouseleave", event => {
+	b.classList.remove('animated', 'heartBeat')
+	});
+
+c.addEventListener ("mouseenter", event => {
+	c.classList.add('animated', 'heartBeat')
+	});
+
+c.addEventListener ("mouseleave", event => {
+	c.classList.remove('animated', 'heartBeat')
+	});
+
+d.addEventListener ("mouseenter", event => {
+	d.classList.add('animated', 'heartBeat')
+	});
+
+d.addEventListener ("mouseleave", event => {
+	d.classList.remove('animated', 'heartBeat')
+	});
+
+e.addEventListener ("mouseenter", event => {
+	e.classList.add('animated', 'heartBeat')
+	});
+
+e.addEventListener ("mouseleave", event => {
+	e.classList.remove('animated', 'heartBeat')
+	});
+
+f.addEventListener ("mouseenter", event => {
+	f.classList.add('animated', 'heartBeat')
+	});
+
+f.addEventListener ("mouseleave", event => {
+	f.classList.remove('animated', 'heartBeat')
 	});
